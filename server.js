b635fe342bc024301dd83dcb94b79b3cd860bcba
Endrit Kastrati <endritkastrati8@gmail.com>
2024-11-08 17:17:22 +0100

    Making server needed variables (port, max_conn, timer, file_location)

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const dgram = require('dgram');
 const fs = require('fs');
 const path = require('path');
 const server = dgram.createSocket('udp4');
+
+const PORTI = 40000;
+const MAX_KONEKTIME = 1;
+const KOHA_JOAKTIVE = 20000;
+const LOKACIONI_FAJLLAV = './test-files';
+const FAJLL_INFO = path.join(LOKACIONI_FAJLLAV, 'example.txt');
