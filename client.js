fd4c8696a825c50ba6e00b63f803b224988e0102
elisagashi <elisagashi99@gmail.com>
2024-11-09 20:27:24 +0200

    fillimi

--- client.js
+++ client.js
@@ -2,3 +2,10 @@ const dgram = require('dgram');
 const readline = require('readline');
 const client = dgram.createSocket('udp4');
 
+const SERVER_PORT = 40000;
+const SERVER_IP = '127.0.0.1';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
