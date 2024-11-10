ec7ba4918f0ae756ac727d296f5718b83fd3024c
Endrit Kastrati <endritkastrati8@gmail.com>
2024-11-10 15:04:15 +0100

    Final change / testing done.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const server = dgram.createSocket('udp4');
 
 const PORTI = 40000;
-const MAX_KONEKTIME = 1;
+const MAX_KONEKTIME = 4;
 const KOHA_JOAKTIVE = 20000;
 const LOKACIONI_FAJLLAV = './test-files';
 const FAJLL_INFO = path.join(LOKACIONI_FAJLLAV, 'example.txt');
